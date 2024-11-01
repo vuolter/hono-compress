57872cc3a8d88090449c23b20eba486fc26d5581
Walter Purcaro <vuolter@gmail.com>
2024-11-01 12:24:09 +0100

    feat: new middleware core with fixed buffer conversion and threshold

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,7 @@
-import type { MiddlewareHandler } from "hono"
-import { gzipSync, deflateSync, type ZlibCompressionOptions } from "bun"
-import { CompressionStream } from "./stream"
-import { isReadableStream } from "./utils"
+import { compress } from "./middleware"
 
-export type CompressionOptions = {
-  /**
-   * @default `gzip`
-   *
-   * Algorithm to use for compression.
-   */
-  type: "gzip" | "deflate" // | 'brotli'
-  /**
-   * @param {Object}
-   *
-   * Options for the compression algorithm.
-   */
-  options?: ZlibCompressionOptions // | BrotliOptions
-  /**
-   * @default `utf-8`
-   *
-   * Encoding for the response body.
-   */
-  encoding?: BufferEncoding
-}
+export * from "./stream"
+export * from "./types"
+export { compress }
 
-const toBuffer = (data: unknown, encoding: BufferEncoding) =>
-  Buffer.from(
-    typeof data === "object"
-      ? JSON.stringify(data)
-      : (data?.toString() ?? new String(data)),
-    encoding,
-  )
-
-export const compress = (
-  { type = "gzip", options = {}, encoding = "utf-8" }: CompressionOptions = {
-    type: "gzip",
-    encoding: "utf-8",
-  },
-): MiddlewareHandler => {
-  if (!["gzip", "deflate"].includes(type)) {
-    throw new Error("Invalid compression type. Use gzip or deflate.")
-  }
-
-  return async function compress(c, next) {
-    await next()
-    const accepted = c.req.header("Accept-Encoding")
-    const acceptsEncoding = accepted?.includes(type)
-
-    if (!acceptsEncoding || !c.res.body) {
-      return
-    }
-
-    const stream = c.res.body
-    const compressedBody = isReadableStream(stream)
-      ? stream.pipeThrough(new CompressionStream(type))
-      : type === "gzip"
-        ? gzipSync(toBuffer(c.res.body, encoding), options)
-        : deflateSync(toBuffer(c.res.body, encoding), options)
-
-    c.res = new Response(compressedBody, {
-      headers: c.res.headers,
-    })
-    c.res.headers.set("Content-Encoding", type)
-  }
-}
+export default compress
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,37 @@
+import { ACCEPTED_ENCODINGS } from "./middleware"
 import type { BrotliOptions, ZlibOptions } from "node:zlib"
+import type { ZlibCompressionOptions, LibdeflateCompressionOptions } from "bun"
+
+export type CompressionEncoding = (typeof ACCEPTED_ENCODINGS)[number]
+export type CompressionOptions = ZlibCompressionOptions | LibdeflateCompressionOptions
 export type CompressionStreamOptions = BrotliOptions | ZlibOptions
+
+export interface CompressOptions {
+  /**
+   * Algorithm to use for compression.
+   *
+   * @default `gzip`
+   */
+  type: CompressionEncoding
+
+  /**
+   * Options for the compression algorithm.
+   *
+   * @param {Object}
+   */
+  options?: CompressionOptions
+
+  /**
+   * Options for the stream compression algorithm.
+   *
+   * @param {Object}
+   */
+  streamOptions?: CompressionStreamOptions
+
+  /**
+   * The minimum byte size for a response to be compressed.
+   *
+   * @default 1024
+   */
+  threshold?: number
+}
