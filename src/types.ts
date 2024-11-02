0980b629137a6166f51535672e5d8a6cd83786e2
Walter Purcaro <vuolter@gmail.com>
2024-11-02 10:57:09 +0100

    fix: types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,17 @@
-import { ACCEPTED_ENCODINGS } from './middleware'
+import { NODE_ENCODINGS, WEB_ENCODINGS, ZSTD_ENCODINGS } from './constants'
 import type { BrotliOptions, ZlibOptions } from 'node:zlib'
-import type { ZlibCompressionOptions, LibdeflateCompressionOptions } from 'bun'
 
-export type CompressionEncoding = (typeof ACCEPTED_ENCODINGS)[number]
-export type CompressionOptions = ZlibCompressionOptions | LibdeflateCompressionOptions
-export type CompressionStreamOptions = BrotliOptions | ZlibOptions
+export type ZstdCompressionEncoding = (typeof ZSTD_ENCODINGS)[number]
+
+export type NodeCompressionEncoding = (typeof NODE_ENCODINGS)[number]
+export type NodeCompressionOptions = BrotliOptions | ZlibOptions
+
+export type WebCompressionEncoding = (typeof WEB_ENCODINGS)[number]
+
+export type CompressionEncoding =
+  | ZstdCompressionEncoding
+  | NodeCompressionEncoding
+  | WebCompressionEncoding
 
 export interface CompressOptions {
   /**
@@ -22,18 +29,11 @@ export interface CompressOptions {
   encodings?: CompressionEncoding[]
 
   /**
-   * Options passed to the compression engine to compress fixed-length content.
+   * Options passed to the node compression engine.
    *
    * @param {Object}
    */
-  options?: CompressionOptions
-
-  /**
-   * Options passed to the compression engine to compress streaming content.
-   *
-   * @param {Object}
-   */
-  streamOptions?: CompressionStreamOptions
+  options?: NodeCompressionOptions
 
   /**
    * The minimum size in bytes for a response content to be compressed.
@@ -41,4 +41,11 @@ export interface CompressOptions {
    * @default 1024
    */
   threshold?: number
+
+  /**
+   * Zstandard algorithm compression level
+   *
+   * @default 3
+   */
+  zstdLevel?: number
 }
