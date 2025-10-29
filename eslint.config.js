9ce94f41f523e1c80f323979c6c6618e8a2e715e
Walter Purcaro <vuolter@gmail.com>
2025-10-30 00:14:00 +0100

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,42 @@
-import eslint from '@eslint/js'
+import js from '@eslint/js'
 import prettier from 'eslint-config-prettier'
 import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript'
 import { importX } from 'eslint-plugin-import-x'
-import markdown from 'eslint-plugin-markdown'
+// import markdown from 'eslint-plugin-markdown'
 import perfectionist from 'eslint-plugin-perfectionist'
 import regexp from 'eslint-plugin-regexp'
 import security from 'eslint-plugin-security'
 import sonarjs from 'eslint-plugin-sonarjs'
 import tsdoc from 'eslint-plugin-tsdoc'
 import unicorn from 'eslint-plugin-unicorn'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
+import globals from 'globals'
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  markdown.configs.recommended,
-  unicorn.configs.recommended,
-  regexp.configs['flat/recommended'],
-  importX.flatConfigs.recommended,
-  importX.flatConfigs.typescript,
-  perfectionist.configs['recommended-natural'],
-  security.configs.recommended,
-  sonarjs.configs.recommended,
-  prettier,
-  { ignores: ['dist', 'README.md'] },
-  { plugins: { tsdoc } },
+export default defineConfig([
+  globalIgnores(['.vscode']),
   {
+    files: ['**/*.{ts,tsx}'],
+    plugins: { tsdoc },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      unicorn.configs.recommended,
+      regexp.configs['flat/recommended'],
+      importX.flatConfigs.recommended,
+      importX.flatConfigs.typescript,
+      perfectionist.configs['recommended-natural'],
+      security.configs.recommended,
+      sonarjs.configs.recommended,
+    ],
+    languageOptions: {
+      globals: globals.builtin,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': [
@@ -47,12 +57,17 @@ export default defineConfig(
       'unicorn/prefer-top-level-await': 'off',
       'unicorn/prevent-abbreviations': 'off',
     },
-  },
-  {
     settings: {
       'import-x/resolver-next': [
         createTypeScriptImportResolver({ alwaysTryTypes: true, bun: true }),
       ],
     },
   },
-)
+  // {
+  //   files: ['**/*.md'],
+  //   extends: [markdown.configs.recommended],
+  // },
+  {
+    extends: [prettier],
+  },
+])
